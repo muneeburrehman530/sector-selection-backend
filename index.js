ebd6f8dea612b83da14b5484b8477b69975f990f
muneeb ur rehman <rehmanmuneeb530@gmail.com>
2023-12-28 22:53:32 +0500

    sever file test

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-
-// server.use(middlewares);
-// server.use(
-//   jsonServer.rewriter({
-//     "/*": "/$1",
-//   })
-// );
-// server.use(router);
-
-// const PORT = process.env.PORT || 3001;
-
-// server.listen(PORT, (err) => {
-//   if (err) {
-//     console.error("Error starting server:", err);
-//   } else {
-//     console.log(`JSON Server is running on port ${PORT}`);
-//   }
-// });
 const jsonServer = require("json-server");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const cors = require("cors"); // Import cors middleware
 
 const adapter = new FileSync("db.json"); // Path to your JSON file
 const db = low(adapter);
@@ -32,6 +11,19 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Enable CORS for all routes
+server.use(cors());
+
+// Allow CORS for specific origins (e.g., localhost)
+// Replace 'http://localhost:3000' with your frontend's URL
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // Enable credentials (if needed)
+  })
+);
+
 server.use(router);
 
 const PORT = process.env.PORT || 3001;
